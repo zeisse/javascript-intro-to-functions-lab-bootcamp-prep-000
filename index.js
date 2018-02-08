6a53c84c497766d9356b3ed7f81f459fc935d265
Brook Zeisse <b@zeisse.com>
2018-02-08 21:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
- return string.toLowerCase()
- return string.toUpperCase()
- return string ='I love you, Grandma.'
+ return string.toLowerCase(), string.toUpperCase(), string ='I love you, Grandma.'
 }
 
 
